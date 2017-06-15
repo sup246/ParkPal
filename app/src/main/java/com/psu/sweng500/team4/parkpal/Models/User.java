7a3980564718128b62704ac8a1c346431bfee399
brhoads_5133 <brhoads5133@gmail.com>
2017-06-14 20:27:00 -0400

    +User encapsulation

--- app/src/main/java/com/psu/sweng500/team4/parkpal/Models/User.java
+++ app/src/main/java/com/psu/sweng500/team4/parkpal/Models/User.java
@@ -24,6 +24,62 @@ public class User {
     private String id;
     public String getId() { return id; }
     public final void setId(String id) { this.id = id; }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getBirthdate() {
+        return birthdate;
+    }
+
+    public void setBirthdate(String birthdate) {
+        this.birthdate = birthdate;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipCode(String zipCode) {
+        this.zipCode = zipCode;
+    }
 }
 
 
